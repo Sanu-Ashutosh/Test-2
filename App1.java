eeeef9ab40b74217171f3539a3553636522a226a
Ashutosh Pradhan <sanu.ashutoshpradhan@gmail.com>
2024-08-05 15:32:41 +0530

    Exception check

--- App1.java
+++ App1.java
@@ -6,5 +6,18 @@ public class App1 {
     public static void main(String[] args) {
         
         System.out.println("Hellow world");
+        try {
+            System.out.println("before exception");
+            int a=28/0;
+            System.out.println("After exception");}
+         catch (ArithmeticException e) {
+            System.out.println("in arithmetic");
+            e.printStackTrace();
+            // TODO: handle exception
+        }
+        catch(Exception e ){
+            System.out.println("In exception");
+            e.printStackTrace();
+        } 
     }
 }
